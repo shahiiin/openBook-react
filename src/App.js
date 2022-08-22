168f35669dc8268a5391bdebf4bca81673386321
shahin <shahin.shahi1986@gmail.com>
2022-08-22 09:36:40 +0300

    make main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import Main from './components/Main'
 
 function App() {
   return (
     <div className="App">
-
+      <Main />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,31 @@
 import React from 'react'
+import Book from '../assets/images/bg2.png'
+import {AiOutlineSearch} from 'react-icons/ai'
+
+
+
 
 const Main = () => {
+
   return (
     <>
       <div className="header">
         <div className="row1">
-
+          <h1>
+            A room without books is like
+            <br /> a body without a soul.
+          </h1>
         </div>
         <div className="row2">
-            
-            </div>
+          <h2>Find Your Book</h2>
+          <div className="search">
+            <input type="text" placeholder="Find Your Book Name" />
+            <button>
+              <AiOutlineSearch/>
+            </button>
+          </div>
+          <img src={Book} alt='Book'/>
+        </div>
       </div>
     </>
   )
