adcb6ef3f85c4663ca09a00a8119ab4008e0d1f8
shahin <shahin.shahi1986@gmail.com>
2022-08-22 08:33:51 +0300

    start ui of simple project

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="row1">
+
+        </div>
+        <div className="row2">
+            
+            </div>
+      </div>
+    </>
+  )
+}
+
+export default Main
